fbddc3359c61d1e36fa97fc8978626d4cd777e75
KKK <dam3r@gmail.com>
2014-04-25 16:26:35 +0200

    Dojedz do wiezy i stoj

--- Krak2014/src/com/calki/krak/Eliminacje.java
+++ Krak2014/src/com/calki/krak/Eliminacje.java
@@ -1,10 +1,37 @@
 package com.calki.krak;
 
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+
 public class Eliminacje {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	
+	public static void jedzDoPrzodu(){
+		Motor.A.forward();
+		Motor.C.forward();
+	}
+	
+	public static void stop(){
+		Motor.A.stop();
+		Motor.C.stop();
+	}
+	   
+	public static void jedzDoWiezy() throws InterruptedException{
+		UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
+		while(us.getDistance()>5){
+			if(us.getDistance()<7){				
+				stop();
+				break;
+			}
+		      jedzDoPrzodu();
+		   }
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		jedzDoWiezy();
 	}
 
 }
