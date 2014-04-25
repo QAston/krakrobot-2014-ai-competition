e85f7d7fbeedb7e8e9254324837ac69554ebccb9
QAston <qaston@gmail.com>
2014-04-25 19:22:19 +0200

    Final

---
+++ Krak2014/src/com/calki/krak/RelativeDirection.java
@@ -0,0 +1,21 @@
+package com.calki.krak;
+
+public enum RelativeDirection {
+    FRONT(0),
+    LEFT(1),
+    RIGHT(2),
+    BACK(3);
+		
+		private int index;
+		
+		RelativeDirection(int index)
+		{
+			this.index = index;
+		}
+		
+		int getIndex()
+		{
+			return index;
+		}
+
+}
