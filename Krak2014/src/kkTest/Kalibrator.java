e98c344ca4d429dbaae9678a84217b5a95a263c7
Khanrad <mgkonrad@gmail.com>
2014-04-25 18:22:13 +0200

    kalibrator kolor

--- Krak2014/src/kkTest/Kalibrator.java
+++ Krak2014/src/kkTest/Kalibrator.java
@@ -1,8 +1,10 @@
 package kkTest;
 
 import lejos.nxt.Button;
+import lejos.nxt.ColorSensor;
 import lejos.nxt.LCD;
 import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
 import lejos.robotics.localization.OdometryPoseProvider;
 import lejos.robotics.navigation.*;
 
@@ -15,15 +17,25 @@ public class Kalibrator {
 		 
 		 DifferentialPilot pilot = new DifferentialPilot(8.3d,8.18d,19.2d,Motor.C,Motor.A,true);
 	     OdometryPoseProvider opp = new OdometryPoseProvider(pilot);
-	     
+	     ColorSensor kolor = new ColorSensor(SensorPort.S2);
+	    
 	     
 	     pilot.setTravelSpeed(15);
-	     pilot.setRotateSpeed(45);
-	     //pilot.setAcceleration(10);
+	     pilot.setRotateSpeed(35);
 	     
-	     Pose pose = opp.getPose();
 	     
-	     LCD.drawString(Double.toString(pose.getLocation().getX()), 0, 0);
+	     Pose OldPose = opp.getPose();
+	     Pose NewPose;
+	     
+	     while(kolor.getColorID() != 7){
+	    	 pilot.rotateRight();
+	     }
+	     NewPose = opp.getPose();
+	     
+	     float diff = OldPose.getHeading() - NewPose.getHeading();
+	     
+	     pilot.rotate((double)diff);
+	     /*LCD.drawString(Double.toString(pose.getLocation().getX()), 0, 0);
 	     LCD.drawString(Double.toString(pose.getLocation().getY()), 0, 1);
 	     LCD.drawString(Float.toString(pose.getHeading()), 0, 2);
 	     
@@ -50,7 +62,9 @@ public class Kalibrator {
 	     
 	     LCD.drawString(Double.toString(pose.getLocation().getX()), 0, 3);
 	     LCD.drawString(Double.toString(pose.getLocation().getY()), 0, 4);
-	     LCD.drawString(Float.toString(pose.getHeading()), 0, 5);
+	     LCD.drawString(Float.toString(pose.getHeading()), 0, 5);*/
+	     
+	     
 	     Button.waitForAnyPress();
 	 }
 
