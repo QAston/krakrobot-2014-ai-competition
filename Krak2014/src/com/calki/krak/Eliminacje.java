0307fae119b51f9b18431e85367f51c15c4f41ae
KKK <dam3r@gmail.com>
2014-04-25 19:24:44 +0200

    Wypisanie mapy na ekran, update mapy ( odwrotna )

--- Krak2014/src/com/calki/krak/Eliminacje.java
+++ Krak2014/src/com/calki/krak/Eliminacje.java
@@ -8,15 +8,26 @@ import lejos.nxt.SensorConstants;
 import lejos.nxt.SensorPort;
 import lejos.nxt.Sound;
 import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.localization.OdometryPoseProvider;
 import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.navigation.Pose;
 
 public class Eliminacje {
 	public static DifferentialPilot pilot;
 	public static UltrasonicSensor ultraSensor;
 	public static ColorSensor colorSensor;
 	
+	public static OdometryPoseProvider opp;
+	public static Pose pose;
+	
 	public static int BIALY = 6;
 	public static int NIEBIESKI = 2;
+	public static int CZARNY = 7;
+	
+	public static int posX = 4;
+	public static int posY = 4;
+	
+	public static int mapa[][];
 	
 	public static void lcdPokazOdleglosc(){
 		   while(true){
@@ -34,6 +45,19 @@ public class Eliminacje {
 		    LCD.drawInt(value, 0, 3);
 		    LCD.refresh();
 		}
+	}
+	
+	public static void lcdPokazMape(){
+			LCD.clear();
+			int i,j;
+			for(i=0;i<=4;i++){
+				for(j=0;j<=4;j++){
+					LCD.drawInt(mapa[i][i], i, j);
+				}
+			}
+			LCD.drawInt(posX, 5, 1);
+			LCD.drawInt(posY, 6, 1);
+			Button.waitForAnyPress();
 	}
 	
 	public static void kolorOpusc(){
@@ -57,10 +81,12 @@ public class Eliminacje {
 					
 					if(colorSensor.getColorID()==NIEBIESKI){
 						Sound.beep();
+						mapaUstawWieze(NIEBIESKI);
 						pilot.rotate(-pilot.getAngleIncrement());
 						break;
 					}else if(colorSensor.getColorID()==BIALY){
 						Sound.twoBeeps();
+						mapaUstawWieze(BIALY);
 						pilot.rotate(-pilot.getAngleIncrement());
 						break;
 					}
@@ -75,17 +101,37 @@ public class Eliminacje {
 		   }
 	}
 	
-	public static void szukajWiezy(){
-		//while()
+	public static void mapaUstawWieze(int typWiezy){
+		float obecnaPozycjaX=pose.getX();
+		float obecnaPozycjaY=pose.getY();
+		
+		mapa[(int)obecnaPozycjaX%32][(int)obecnaPozycjaY%32]=typWiezy;
+	}
+	
+	public static void szukajWiezy() throws InterruptedException{
+		if(ultraSensor.getDistance()>20 && ultraSensor.getDistance()<75){
+			jedzDoWiezy();
+		}
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
 		pilot = new DifferentialPilot(8.3d,8.18d,19.2d,Motor.C,Motor.A,true);	  
 		ultraSensor = new UltrasonicSensor(SensorPort.S1);
-		colorSensor = new ColorSensor(SensorPort.S4, SensorConstants.TYPE_COLORFULL);
+		colorSensor = new ColorSensor(SensorPort.S4);
 		pilot.setTravelSpeed(15);
 	    pilot.setRotateSpeed(45);
+	    mapa = new int[][]{
+	    		  { 0, 0, 0, 0 },
+	    		  { 0, 0, 0, 0 },
+	    		  { 0, 0, 0, 0 },
+	    		  { 0, 0, 0, 0 },
+	    		  { 0, 0, 0, 0 }
+	    		};
+	    pose = opp.getPose();
+	    
+	    
 		jedzDoWiezy();
+		lcdPokazMape();
 	    //lcdPokazOdleglosc();
 	    //lcdPokazColorId();
 	}
