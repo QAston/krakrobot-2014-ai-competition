6d40e4172a06cc1ec6567f1730deac6d28c5c65b
QAston <qaston@gmail.com>
2014-04-27 07:22:13 +0200

    poprawki

--- Krak2014/src/com/calki/krak/Final.java
+++ Krak2014/src/com/calki/krak/Final.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
+import lejos.nxt.Sound;
 import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.localization.OdometryPoseProvider;
 import lejos.robotics.navigation.DifferentialPilot;
@@ -58,73 +59,88 @@ public class Final {
 		GlobalDirection aktualnaRotacjaRobotaNaMapie = GlobalDirection.NORTH;
 		Map map = new Map(mapVals);
 		map.print();
-		p = new MyAstarPathFinder(Position.get(4, 4), GlobalDirection.NORTH, Position.get(0,0), map);
-		path = p.getPath();
-		pose=opp.getPose();
-		
 		StanRobota stanRobota = StanRobota.JAZDA_PO_PILKE;
 		
-		//KONIEC SETUP
-		int i=0;
-		
-		while(i <= path.size()){
-			Position aktualnaPozycjaNaMapie = path.get(i);
-			if(ultraSensor.getDistance()>15 && ultraSensor.getDistance()<45){	//1 pole 
-				switch(aktualnaRotacjaRobotaNaMapie){
-				case NORTH:
-					mapVals[(int)pose.getX()/32+1][(int)pose.getY()/32]=FieldType.TOWER;
-					break;
-				case SOUTH:
-					mapVals[(int)pose.getX()/32-1][(int)pose.getY()/32]=FieldType.TOWER;
-					break;
-				case WEST:
-					mapVals[(int)pose.getX()/32][(int)pose.getY()/32-1]=FieldType.TOWER;
-					break;
-				case EAST:
-					mapVals[(int)pose.getX()/32][(int)pose.getY()/32+1]=FieldType.TOWER;
-					break;
-				}
-				//Tutaj ustawianie pozycji
-				p = new MyAstarPathFinder(Position.get(4, 4), GlobalDirection.NORTH, Position.get(0,0), map);
-				path = p.getPath();
-			}
-			
-			if(ultraSensor.getDistance()>48 && ultraSensor.getDistance()<77){	//2 pola
-				switch(aktualnaRotacjaRobotaNaMapie){
-				case NORTH:
-					mapVals[(int)pose.getX()/32+2][(int)pose.getY()/32]=FieldType.TOWER;
-					break;
-				case SOUTH:
-					mapVals[(int)pose.getX()/32-2][(int)pose.getY()/32]=FieldType.TOWER;
-					break;
-				case WEST:
-					mapVals[(int)pose.getX()/32][(int)pose.getY()/32-2]=FieldType.TOWER;
-					break;
-				case EAST:
-					mapVals[(int)pose.getX()/32][(int)pose.getY()/32+2]=FieldType.TOWER;
-					break;
-				}
-			}
-			
-			if(aktualnaPozycjaNaMapie==Position.get(0,0)){
-				
-				//zmiana wartosci pola finalowego
-				stanRobota = StanRobota.JAZDA_Z_PILKA;
-				poleFinalowe = Position.get(4, 4);
-				
-				//czekaj i obrot o 90
-				//czekaj
-	
-				//jazda
-			}
-			
-			pilot.rotate(Eliminacje.setDirection(pose, aktualnaPozycjaNaMapie.getCartesianX(), aktualnaPozycjaNaMapie.getCartesianY() ));
-			//Metoda do zmiany kierunku heading robota
+		while(true)
+		{
+			p = new MyAstarPathFinder(aktualnaPozycjaRobotaNaMapie, aktualnaRotacjaRobotaNaMapie, poleFinalowe, map);
+			path = p.getPath();
 			pose=opp.getPose();
-	    	pilot.travel(Eliminacje.setDistanceToPoint(pose, aktualnaPozycjaNaMapie.getCartesianX(),aktualnaPozycjaNaMapie.getCartesianY()));
-	    	pose=opp.getPose();
-	    	Eliminacje.lcdPokazMape();
-			i++;
+			
+			//KONIEC SETUP
+			int i=0;
+			
+			petla_sciezki: while(i <= path.size()){
+				Position docelowaPozycjaNaMapie = path.get(i);
+				// wykryto wieze na polu z przodu
+				if(ultraSensor.getDistance()>15 && ultraSensor.getDistance()<45){	//1 pole 
+					switch(aktualnaRotacjaRobotaNaMapie){
+					case NORTH:
+						mapVals[(int)pose.getX()/32+1][(int)pose.getY()/32]=FieldType.TOWER;
+						break;
+					case SOUTH:
+						mapVals[(int)pose.getX()/32-1][(int)pose.getY()/32]=FieldType.TOWER;
+						break;
+					case WEST:
+						mapVals[(int)pose.getX()/32][(int)pose.getY()/32-1]=FieldType.TOWER;
+						break;
+					case EAST:
+						mapVals[(int)pose.getX()/32][(int)pose.getY()/32+1]=FieldType.TOWER;
+						break;
+					}
+					break petla_sciezki; // - wymysl nowa sciezke
+				}
+				
+				// wykryto wieze troche dalej z przodu
+				if(ultraSensor.getDistance()>48 && ultraSensor.getDistance()<77){	//2 pola
+					switch(aktualnaRotacjaRobotaNaMapie){
+					case NORTH:
+						mapVals[(int)pose.getX()/32+2][(int)pose.getY()/32]=FieldType.TOWER;
+						break;
+					case SOUTH:
+						mapVals[(int)pose.getX()/32-2][(int)pose.getY()/32]=FieldType.TOWER;
+						break;
+					case WEST:
+						mapVals[(int)pose.getX()/32][(int)pose.getY()/32-2]=FieldType.TOWER;
+						break;
+					case EAST:
+						mapVals[(int)pose.getX()/32][(int)pose.getY()/32+2]=FieldType.TOWER;
+						break;
+					}
+					break petla_sciezki; // - wymysl nowa sciezke
+				}
+				
+				// wjezdzamy na pole podawania pilki
+				if(docelowaPozycjaNaMapie==Position.get(0,0) && stanRobota == StanRobota.JAZDA_PO_PILKE){
+					
+					//zmiana wartosci pola finalowego
+					stanRobota = StanRobota.JAZDA_Z_PILKA;
+					poleFinalowe = Position.get(4, 4);
+					
+					//czekaj i obrot o 90
+					//czekaj
+					//jazda
+				}
+				
+				// zakonczenie jazdy
+				if(docelowaPozycjaNaMapie==Position.get(4,4) && stanRobota == StanRobota.JAZDA_Z_PILKA){
+					Sound.setVolume(100);
+					Sound.twoBeeps();
+					System.exit(0);
+				}
+				
+				pilot.rotate(Eliminacje.setDirection(pose, docelowaPozycjaNaMapie.getCartesianX(), docelowaPozycjaNaMapie.getCartesianY() ));
+				//Metoda do zmiany kierunku heading robota
+				pose=opp.getPose();
+		    	pilot.travel(Eliminacje.setDistanceToPoint(pose, docelowaPozycjaNaMapie.getCartesianX(),docelowaPozycjaNaMapie.getCartesianY()));
+		    	pose=opp.getPose();
+		    	Eliminacje.lcdPokazMape();
+		    	
+		    	aktualnaRotacjaRobotaNaMapie = aktualnaPozycjaRobotaNaMapie.getLineDirection(docelowaPozycjaNaMapie);
+		    	aktualnaPozycjaRobotaNaMapie = docelowaPozycjaNaMapie;
+		    	
+				i++;
+			}
 		}
 	}
 	
