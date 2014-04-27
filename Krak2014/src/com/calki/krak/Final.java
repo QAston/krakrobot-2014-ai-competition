f2608e314c3a30e0540050f7d7434b08f9b9076d
KKK <dam3r@gmail.com>
2014-04-27 06:17:39 +0200

    Kurwa

--- Krak2014/src/com/calki/krak/Final.java
+++ Krak2014/src/com/calki/krak/Final.java
@@ -3,16 +3,19 @@ package com.calki.krak;
 import java.util.List;
 
 import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.localization.OdometryPoseProvider;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.navigation.Pose;
 
 public class Final {
-
+	public static UltrasonicSensor ultraSensor;
+	
 	final DifferentialPilot pilot;
 	final OdometryPoseProvider opp;
 	final PositionProvider position;
-	final BuiltinAstarPathFinder mapa;
+	//final BuiltinAstarPathFinder mapa;
 	final Ruchacz ruchacz;
 	
 	public static Pose pose;
@@ -25,7 +28,7 @@ public class Final {
 	    pilot.setTravelSpeed(15);
 	    pilot.setRotateSpeed(35);
 		position = new PositionProviderImpl(opp);
-		mapa = new BuiltinAstarPathFinder();
+		//mapa = new BuiltinAstarPathFinder();
 		
 		ruchacz = new RuchaczImpl(pilot, opp);
 	}
@@ -49,33 +52,39 @@ public class Final {
 					FieldType.UNKNOWN, FieldType.UNKNOWN, FieldType.UNKNOWN, FieldType.UNKNOWN, FieldType.UNKNOWN
 				}
 		};
-		
+		Position aktualnaPozycjaRobotaNaMapie = Position.get(4, 4);
+		GlobalDirection aktualnaRotacjaRobotaNaMapie = GlobalDirection.NORTH;
 		Map map = new Map(mapVals);
-		//map.print();
+		map.print();
 		PathFinder p = new MyAstarPathFinder(Position.get(4, 4), GlobalDirection.NORTH, Position.get(0,0), map);
 		List<Position> path = p.getPath();
-		/*pose=opp.getPose();
+		pose=opp.getPose();
+		
 		int i=0;
 		
 		while(i <= path.size()){
 			Position aktualnaPozycjaNaMapie = path.get(i);
-			pilot.rotate(Eliminacje.setDirection(pose, aktualnaPozycjaNaMapie.getCartesianX(),aktualnaPozycjaNaMapie.getCartesianY()));
+			if(ultraSensor.getDistance()>15 && ultraSensor.getDistance()<45){	//1 pole 
+				
+				//int direction = pose.getHeading();
+				//mapVals[(int)pose.getX()/32][(int)pose.getY()/32]
+			}
+			
+			if(ultraSensor.getDistance()>48 && ultraSensor.getDistance()<77){	//2 pola
+				//mapVals[(int)pose.getX()/32][(int)pose.getY()/32]
+			}
+			
+			pilot.rotate(Eliminacje.setDirection(pose, aktualnaPozycjaNaMapie.getCartesianX(), aktualnaPozycjaNaMapie.getCartesianY() ));
 			pose=opp.getPose();
 	    	pilot.travel(Eliminacje.setDistanceToPoint(pose, aktualnaPozycjaNaMapie.getCartesianX(),aktualnaPozycjaNaMapie.getCartesianY()));
 	    	pose=opp.getPose();
 			i++;
-		}*/
-		
-		
-		while(true)
-		{
-			ruchacz.forward();
 		}
 	}
 	
 	
 	public static void main(String[] args) {
-
+		//ultraSensor = new UltrasonicSensor(SensorPort.S1);
 	    Final f = new Final();
 	    f.run();
 	}
